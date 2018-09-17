58482c13af368a69c53c8231af90adc248bf7257
Scott Weinstein <sweinstein27@gmail.com>
2018-09-17 17:36:12 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,13 @@
 class ArtistsController < ApplicationController
   def index
+    @artist = Artist.all
   end
 
   def show
-    binding.pry
+    @artist = Artist.find(params[:id])
   end
 
   def new
-    binding.pry
     @artist = Artist.new
   end
 
